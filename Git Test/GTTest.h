91f05215a940855496e27eba996044bd7a5ecec0
Daniel Chou <dechou100@gmail.com>
2014-08-27 23:37:43 -0700

    added a test class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by D.C. on 8/27/14.
+//  Copyright (c) 2014 DeChou100Production. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by D.C. on 8/27/14.
+//  Copyright (c) 2014 DeChou100Production. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
